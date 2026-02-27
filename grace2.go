b6810b242aea0f048f9ed148dadc095e548d50dc
oomale <omalegrace2009@gmail.com>
2026-02-27 16:37:51 +0000

    practice

---
+++ grace2.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var grace string
+	fmt.Println("Enter your name:")
+	fmt.Scan(&grace)
+	var peace int
+	if grace == peace {
+		fmt.Println("correct")
+	} else {
+		fmt.Print("incorrect")
+	}
+}
