6707104cc18e2055b1f71f1848d0d26a3231865b
oomale <omalegrace2009@gmail.com>
2026-06-01 12:00:39 +0100

    working

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"net/http"
+)
 func main() {
-	
+	http.HandleFunc("/", HandleThings)
+	http.ListenAndServe(":8080", nil)
+}
+func HandleThings(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "templates/input.html")
 }
